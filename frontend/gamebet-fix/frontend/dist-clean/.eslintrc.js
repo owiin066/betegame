9e18ab77e438507c879b802604807d094288d4c4
owiin066 <owenmok@hotmail.fr>
2025-04-10 22:48:47 +0200

    Create .eslintrc.js

---
+++ frontend/gamebet-fix/frontend/dist-clean/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // Désactiver les règles qui pourraient causer des problèmes
+    'no-unused-vars': 'off',
+    'vue/no-unused-components': 'off'
+  }
+}
